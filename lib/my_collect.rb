3282033d86e48dbc5b1b900a11d800432321b051
Randy Steele <steelerx155@gmail.com>
2019-10-19 16:52:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -11,4 +11,12 @@ def my_collect(hello)
         empty_array
   end 
 
-  
+  def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
