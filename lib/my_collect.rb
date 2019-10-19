4e2101bb5dd03cc9455c6dc563886817ed3f830c
Randy Steele <steelerx155@gmail.com>
2019-10-19 16:48:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+require 'pry'
+def my_collect(hello)
+  languages = ['ruby', 'javascript', 'python', 'objective-c']
+  students =  ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+   x = 0
+   empty_array = []
+   while x > hello.length
+   empty_array << yield(hello[x])
+      x += 1
+      empty_array << yield(languages[x])
+      languages.upcase
+      x += 1
+        end
+        empty_array
+  end 
 
-
+  
