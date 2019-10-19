6b8d7c80ba3fdec979934a5bb42b437f8ce22e08
Randy Steele <steelerx155@gmail.com>
2019-10-19 16:54:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,15 @@
-require 'pry'
-def my_collect(hello)
-     x = 0
-   empty_array = []
-   while x < hello.length
-   empty_array << yield(hello[x])
-      x += 1
-      empty_array << yield(languages.upcase[x])
-      x += 1
-        end
-        empty_array
-  end 
+# require 'pry'
+# def my_collect(hello)
+#     x = 0
+#   empty_array = []
+#   while x < hello.length
+#   empty_array << yield(hello[x])
+#       x += 1
+#       empty_array << yield(languages.upcase[x])
+#       x += 1
+#         end
+#         empty_array
+#   end 
 
   def my_collect(collection)
   i = 0
